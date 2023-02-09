ab15eaff5fb29baf3b0bd73fadece72896e45f48
joycehwchan <joycehwchan@gmail.com>
2023-02-09 16:06:00 +0900

    initialize project

---
+++ src/main/java/com/joycehwchan/diningreviews/DiningreviewsApplication.java
@@ -0,0 +1,13 @@
+package com.joycehwchan.diningreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningreviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningreviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joycehwchan/diningreviews/DiningreviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joycehwchan.diningreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningreviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
