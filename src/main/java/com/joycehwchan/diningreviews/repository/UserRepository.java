7274f67f47feb2762d031bfaf51238a055868398
joycehwchan <joycehwchan@gmail.com>
2023-02-10 12:29:39 +0900

    add custom user query

--- src/main/java/com/joycehwchan/diningreviews/repository/UserRepository.java
+++ src/main/java/com/joycehwchan/diningreviews/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.joycehwchan.diningreviews.repository;
 import com.joycehwchan.diningreviews.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
