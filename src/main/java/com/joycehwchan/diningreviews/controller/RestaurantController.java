d4834816c6ff1b6e750afcf435fcdf700f79d552
joycehwchan <joycehwchan@gmail.com>
2023-02-10 12:29:07 +0900

    getAll and post restaurant

--- src/main/java/com/joycehwchan/diningreviews/controller/RestaurantController.java
+++ src/main/java/com/joycehwchan/diningreviews/controller/RestaurantController.java
@@ -1,8 +1,10 @@
 package com.joycehwchan.diningreviews.controller;
 
+import com.joycehwchan.diningreviews.model.Restaurant;
 import com.joycehwchan.diningreviews.repository.RestaurantRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/restaurants")
@@ -11,5 +13,17 @@ public class RestaurantController {
 
     public RestaurantController(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
+    }
+
+    @GetMapping
+    public Iterable<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+        // add validations...
+        return restaurantRepository.save(restaurant);
     }
 }
