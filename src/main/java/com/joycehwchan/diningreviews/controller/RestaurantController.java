c277e3438bb6a0d46872793c8a0f66eea2feaefe
joycehwchan <joycehwchan@gmail.com>
2023-02-11 12:10:02 +0900

    add validations for addRestaurant

--- src/main/java/com/joycehwchan/diningreviews/controller/RestaurantController.java
+++ src/main/java/com/joycehwchan/diningreviews/controller/RestaurantController.java
@@ -3,16 +3,19 @@ package com.joycehwchan.diningreviews.controller;
 import com.joycehwchan.diningreviews.model.Restaurant;
 import com.joycehwchan.diningreviews.repository.RestaurantRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 
 @RestController
 @RequestMapping("/restaurants")
 public class RestaurantController {
     private final RestaurantRepository restaurantRepository;
+    private final Pattern zipCodePattern = Pattern.compile("\\d{5}");
 
     public RestaurantController(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
@@ -35,7 +38,26 @@ public class RestaurantController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
-        // add validations...
+        validate(restaurant);
         return restaurantRepository.save(restaurant);
+    }
+
+    private void validate(Restaurant restaurant) {
+        if (ObjectUtils.isEmpty(restaurant.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        validateZipCode(restaurant.getZipCode());
+
+        Optional<Restaurant> existingRestaurant = restaurantRepository.findByNameAndZipCode(restaurant.getName(), restaurant.getZipCode());
+        if (existingRestaurant.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+    }
+
+    private void validateZipCode(String zipCode) {
+        if (!zipCodePattern.matcher(zipCode).matches()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/joycehwchan/diningreviews/repository/RestaurantRepository.java
+++ src/main/java/com/joycehwchan/diningreviews/repository/RestaurantRepository.java
@@ -3,5 +3,8 @@ package com.joycehwchan.diningreviews.repository;
 import com.joycehwchan.diningreviews.model.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+    Optional<Restaurant> findByNameAndZipCode(String name, String zipCode);
 }
