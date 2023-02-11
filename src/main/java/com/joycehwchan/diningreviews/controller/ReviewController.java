e856f7f554007c556faa7c74200abfd7a4cd1920
joycehwchan <joycehwchan@gmail.com>
2023-02-11 14:03:24 +0900

    post review

--- src/main/java/com/joycehwchan/diningreviews/controller/ReviewController.java
+++ src/main/java/com/joycehwchan/diningreviews/controller/ReviewController.java
@@ -1,10 +1,14 @@
 package com.joycehwchan.diningreviews.controller;
 
+import com.joycehwchan.diningreviews.model.Review;
+import com.joycehwchan.diningreviews.model.ReviewStatus;
 import com.joycehwchan.diningreviews.repository.RestaurantRepository;
 import com.joycehwchan.diningreviews.repository.ReviewRepository;
 import com.joycehwchan.diningreviews.repository.UserRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reviews")
@@ -18,5 +22,23 @@ public class ReviewController {
         this.reviewRepository = reviewRepository;
         this.userRepository = userRepository;
         this.restaurantRepository = restaurantRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void addReview(@RequestBody Review review) {
+        validateReview(review);
+
+        review.setStatus(ReviewStatus.PENDING);
+        reviewRepository.save(review);
+    }
+
+    @GetMapping
+    public Iterable<Review> getAllReviews() {
+        return reviewRepository.findAll();
+    }
+
+    private void validateReview(Review review) {
+        // validations...
     }
 }
